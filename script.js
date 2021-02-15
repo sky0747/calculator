a91dcfb4bfa8bcc4c79b81ed8f8defd148079fbb
sky0747 <vova9862003@gmail.com>
2021-02-15 07:00:38 +0200

    changes from me

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function sliderRange() {
+    var rangeValue = document.getElementById("sliderRangeValue");
+    rangeValue.innerHTML = rate.value;
+
 }
-        
+function futureYears() {
+    var years = document.getElementById("years");
+    var years1 = years.value;
+    var futureYears = document.getElementById("futureYears");
+    futureYears.innerHTML = Number(years1) + 2021;
+
+}
+
+
+function compute() {
+
+    var principal = document.getElementById("principal");
+    var principal1 = principal.value;
+    var rate = document.getElementById("rate");
+    var rate1 = rate.value;
+    var years = document.getElementById("years");
+    var years1 = years.value;
+    var interest = (principal1 * years1 * rate1) / 100;
+    var result = document.getElementById("result");
+    result.innerHTML = interest;
+    alert(result.innerHTML);
+
+
+
+
+
+}
+
